22edbf7c34370c2732e1c2dbedf7b58853523fbf
Аделя Галиева <agalieva745gmail.com>
2026-02-26 19:53:11 +0300

    homework 10_1 done homework 10_2 done

--- src/   processing/ processing.py
+++ src/   processing/ processing.py
@@ -10,3 +10,18 @@ def filter_by_state(data, state='EXECUTED'):
     :return: отфильтрованный список словарей
     """
     return [item for item in data if item.get('state') == state]
+
+
+def sort_by_date(data, descending=True):
+    """
+    Сортировка данных по дате
+
+    :param data: список словарей с данными
+    :param descending: флаг сортировки (True - убывание, False - возрастание)
+    :return: отсортированный список словарей
+    """
+    return sorted(
+        data,
+        key=lambda x: datetime.strptime(x['date'], '%Y-%m-%dT%H:%M:%S.%f'),
+        reverse=descending
+    )
